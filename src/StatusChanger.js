313c21325ee0659264d65308c095399485791fef
anvorgesa <95182588+anvorgesa@users.noreply.github.com>
2020-05-27 04:38:14 -0300

    added source

---
+++ src/StatusChanger.js
@@ -0,0 +1,44 @@
+var Status = require('./Status')
+
+function StatusChanger(presence) {
+    this.presence = presence
+}
+
+StatusChanger.prototype = {
+    apply: function(game) {
+        this.presence.setGame(game)
+    },
+    build: function(obj) {
+        var arr = []
+        var playing = obj.playing
+        var listening = obj.listening
+        var watching = obj.watching
+
+        if (playing) {
+            for (var i = 0; i < playing.length; i++) {
+                var text = playing[i]
+                arr.push(new Status('DEFAULT', text))
+            }
+        }
+
+        if (listening) {
+            for (var i = 0; i < listening.length; i++) {
+                var text = listening[i];
+                arr.push(new Status('LISTENING', text))
+            }
+        }
+
+        if (watching) {
+            for (var i = 0; i < watching.length; i++) {
+                var text = watching[i];
+                arr.push(new Status('WATCHING', text))
+            }
+        }
+
+        return arr;
+    }
+}
+
+module.exports = function(jda) {
+    return new StatusChanger(jda.getPresence())
+}
